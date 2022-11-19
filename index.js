deec6e0aad298aab1c06e8c9d6cae3f9d3fdf4ec
Lucas Bezerra Matias <100497621+lucasbm2022@users.noreply.github.com>
2022-11-19 00:35:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//PageUP
+let btn = document.querySelector("#home");
+
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
+
+//Buttons
+function myFunction() {
+  alert("Entre em contato pelo nosso whatsapp: (81) 99575-6448")
+}
